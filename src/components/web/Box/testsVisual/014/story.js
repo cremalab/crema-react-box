55b7fb6115dedded77c8193b8a232d14fbfa1766
Rob LaFeve <rob@cremalab.com>
2017-04-13 14:32:43 -0500

    Add better use-case to story 14

--- src/components/web/Box/testsVisual/014/story.js
+++ src/components/web/Box/testsVisual/014/story.js
@@ -2,6 +2,19 @@ import React from "react"
 import Box from "../.."
 import { story, css, getName } from '../utils'
 
+const Child = () =>
+<Box childDirection='row' style={{backgroundColor:'white'}}>
+  <Box grow='1' style={{backgroundColor: 'teal'}} padding="10px" >
+    <div>Rob</div>
+    <div>Rob</div>
+    <div>Rob</div>
+    <div>Rob</div>
+    <div>Rob</div>
+    <div>Rob</div>
+  </Box>
+  <Box grow='1' padding="10px">Other stuff aksdfj alksdfj lkasfjoijiw  aksf jaskjf iisa iasjf jkafj</Box>
+</Box>
+
 story.add(getName(__dirname), () => {
     return <Box
       padding="20px"
@@ -13,18 +26,8 @@ story.add(getName(__dirname), () => {
       childAlign="flex-start"
       childDirection="row"
       style={css.parent}>
-      {[...Array(80).keys()].map((x, i) =>
-        <Box grow='1' key={i} childDirection='row' style={{backgroundColor:'white'}}>
-          <Box style={{backgroundColor: 'teal'}} padding="10px" width="150px" >
-            <div>Rob</div>
-            <div>Rob</div>
-            <div>Rob</div>
-            <div>Rob</div>
-            <div>Rob</div>
-            <div>Rob</div>
-          </Box>
-          <Box grow='1' padding="10px">Other stuff aksdfj alksdfj lkasfjoijiw  aksf jaskjf iisa iasjf jkafj</Box>
-        </Box>
+      {[...Array(10).keys()].map((x, i) =>
+        <Child grow='1' key={i}/>
       )}
     </Box>
   })
