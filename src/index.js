c9c6558b2e6513166dc5cf2c42f1e5732a17c7b4
Rob LaFeve <rob@cremalab.com>
2017-04-17 15:37:32 -0500

    Check for undefined

--- src/index.js
+++ src/index.js
@@ -24,7 +24,13 @@ const spacerPadding = p =>
     ? propStyle('childSpacing', { halve: true })
     : 0
 
-const onlyText = x => x.length && x.map(x => typeof x === 'string' || typeof x === 'number').indexOf(false) === -1
+const onlyText = x =>
+  x &&
+  x.length &&
+  x.map(x =>
+    typeof x === 'string' ||
+    typeof x === 'number'
+  ).indexOf(false) === -1
 
 const Container = styled.div`
   box-sizing:      border-box;
